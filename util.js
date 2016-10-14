ccbe80dfc4f1c987e4038f92d1d95c12707b7f82
zktree <zktree@163.com>
2016-10-14 23:26:28 +0800

    Create util.js

---
+++ util.js
@@ -0,0 +1,18 @@
+var srcObj = {
+    a: 1,
+    b: {
+        b1: ["hello", "hi"],
+        b2: "JavaScript"
+    }
+};
+var abObj = srcObj;
+var tarObj = cloneObject(srcObj);
+
+srcObj.a = 2;
+srcObj.b.b1[0] = "Hello";
+
+console.log(abObj.a);
+console.log(abObj.b.b1[0]);
+
+console.log(tarObj.a);      // 1
+console.log(tarObj.b.b1[0]);    // "hello"
